2c87acdb0335b4f10cf9e9f72927075d36373ecc
EC2 Default User <ec2-user@ip-172-31-37-86.us-west-2.compute.internal>
2018-06-20 15:41:39 +0000

    Auth e home page adicionadas

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col col-lg-2">
+               <h1 class="text-center">Cheetah</h1>
+            </div>
+        </div>
+        <div class="row justify-content-md-center">
+            <div class="col col-lg-6">
+               <img src="{{ asset('img/cheetah.png') }}" class="img-fluid" alt="Responsive image">
+            </div>
+        </div>
+        <div class="row">
+
+    </div>
+   
+@endsection
